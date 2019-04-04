2bc9b828c34eb3bce0d2db0589423b1d9ab5dfe5
Lauren <laurentepaske@gmail.com>
2019-04-03 17:59:51 -0700

    trying again

--- deploy.js
+++ deploy.js
@@ -85,9 +85,9 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '00.00.00.00',
+      host: '3.95.217.123',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: 'na-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
